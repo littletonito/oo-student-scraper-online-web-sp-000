820de9ef1a13af8974d010accb3dc7eaedbfc3b6
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-20 22:34:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,9 +13,9 @@ class Scraper
         student_location = student.css('.student-location').text
         student_name = student.css('.student-name').text
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
-      
       end
     end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
