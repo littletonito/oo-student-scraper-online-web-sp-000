dcdcf0980540bc932134cc78307a0fe05759defb
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-20 22:23:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,9 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index_url = https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
     index_page = Nokogiri::HTML(open(index_url))
     students = []
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
