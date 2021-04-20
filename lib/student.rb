f499f9565e4e7254f3a55e9a343a7ff928a06718
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-20 22:49:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -28,4 +28,3 @@ class Student
     @@all
   end
 end
-
